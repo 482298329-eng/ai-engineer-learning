4dd84cf9cc60e5cab8d1ee25f90ec26c67f60d62
482298329-eng <482298329-eng@users.noreply.github.com>
2026-09-15 23:49:48 +0800

    Add common data types example and ignore IDE settings

--- week1/test1.py
+++
@@ -1,5 +0,0 @@
-num=114.1
-print(num)
-
-num=num+1
-print(num)
---
+++ week1/常见数据类型.py
@@ -0,0 +1,22 @@
+# num=114.1
+# print(num)
+#
+# num=num+1
+# print(num)
+
+# base=20.7
+# incr=50
+# print(base+incr)
+# print(base-incr)
+
+#转义字符\' \" \n \t
+mg='it\'s very good'
+print(mg)
+
+mg1="it's very good"
+print(mg1)
+
+mg2="Hello 的意思是\"你好\""
+print(mg2)
+
+print("\t欢迎\n\t大家")#\n换行 \t缩进
